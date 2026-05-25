8aa9e8ec04c76996654c3cc8156d4a35a12e9eba
Ibrahim  Amenzou <ibrahimamenzou@macbook-air-de-ibrahim3.home>
2026-05-25 22:58:15 +0200

    Remove Gmail scopes - use only Calendar (emails via Brevo)

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -2,8 +2,6 @@ import { google } from 'googleapis';
 import { getGoogleTokens, saveGoogleTokens } from './db';
 
 const SCOPES = [
-  'https://www.googleapis.com/auth/gmail.send',
-  'https://www.googleapis.com/auth/gmail.readonly',
   'https://www.googleapis.com/auth/calendar',
   'https://www.googleapis.com/auth/calendar.events',
 ];
